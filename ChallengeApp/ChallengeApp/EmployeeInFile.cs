a0113c20bd097a15e250d89f309e901a6f0a7b3a
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-07 20:16:12 +0200

    Day 16 - base class and abstract method

---
+++ ChallengeApp/ChallengeApp/EmployeeInFile.cs
@@ -0,0 +1,39 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInFile : EmployeeBase
+    {
+        public EmployeeInFile(string name, string surname) : base(name, surname)
+        {
+
+        }
+        public override void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(string grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
