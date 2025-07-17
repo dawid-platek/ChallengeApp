3d93827868683aee2f51a2a24beba6a9126bd981
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-17 15:37:36 +0200

    Day 4: integers

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,3 +1,19 @@
-﻿var myAge = 33;
+﻿using System.Reflection.Metadata.Ecma335;
 
-int myAge2 = 33;
+var myAge = 33;
+
+int myAge2 = 120;
+int newAge = myAge2 + 5;
+Console.WriteLine(newAge);
+
+int myVar = int.MinValue;
+int myVar2 = int.MaxValue;
+
+uint myUVar = uint.MinValue;
+uint myUVar2 = uint.MaxValue;
+
+long myVar3 = long.MinValue;
+long myVar4 = long.MaxValue;
+
+ulong myUVar3 = ulong.MinValue;
+ulong myUVar4 = ulong.MaxValue;
