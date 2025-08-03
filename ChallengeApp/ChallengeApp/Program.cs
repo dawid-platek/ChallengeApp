729dcc89c757e8fc399cbe30b701548562a32c25
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-08-03 20:36:21 +0200

    Day 4: IF statement and debugging basics

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,52 +1,19 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿var number1 = 50;
+var number2 = 10;
 
-var myAge = 33;
+if (number1 > number2)
+{
+    Console.WriteLine($"Liczba {number1} jest większa od liczby {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Liczba {number1} jest mniejsza od liczby {number2}"); ;
+}
+else
+{
+    Console.WriteLine($"Liczba {number1} jest równa liczbie {number2}"); ;
+}
 
-// liczby całkowite
-int myAge2 = 120;
-int newAge = myAge2 + 5;
-Console.WriteLine(newAge);
-
-int myVar = int.MinValue;
-int myVar2 = int.MaxValue;
-
-uint myUVar = uint.MinValue;
-uint myUVar2 = uint.MaxValue;
-
-long myVar3 = long.MinValue;
-long myVar4 = long.MaxValue;
-
-ulong myUVar3 = ulong.MinValue;
-ulong myUVar4 = ulong.MaxValue;
-
-// liczby zmiennoprzecinkowe
-float myNumber = float.MinValue;
-float myNumber2 = float.MaxValue;
-
-double myDouble = double.MinValue;
-double myDouble2 = double.MaxValue;
-
-decimal myDecimal = decimal.MinValue;
-decimal myDecimal2 = decimal.MaxValue;
-
-// zmienne tekstowe
-string name = "Staszek";
-string surname = "Nowak";
-string fullName = name + " " + surname;
-Console.WriteLine(fullName);
-
-//zmienne testowe (char)
-char myChar = 'A';
-var result2 = name.ToArray();
-
-// zmienna logiczna
-bool isTrue = true;
-bool isFalse = false;
-var isValid = 5 > 3;
-Console.WriteLine(isValid);
-
-var isValid2 = 7 < 4;
-Console.WriteLine(isValid2);
 
 
 
